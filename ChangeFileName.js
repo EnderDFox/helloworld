fbedc6f9367176c698227d6b5c7175f316a2b490
EnderDFox <232623741@qq.com>
2017-12-19 22:39:11 +0800

    ChangeFileName.js

--- ChangeFileName.js
+++ ChangeFileName.js
@@ -25,38 +25,63 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser')());
 
+
+var filesData = null;
+
 function getFilesData(){
-    var rs = {};
-    rs.files = [];
-    parseDir("D:/downloads/a3",rs.files);
-    return rs;
+    if(filesData==null){
+        filesData = {};
+        filesData.files = [];
+        parseDir("C:/fox/downloads");
+    }
+    return filesData;
 }
 
-function parseDir(dir,fileFullPaths) {
+function parseDir(dir) {
     var files = fs.readdirSync(dir);
     // console.log("[debug]","files",files.length);
     for (var i = 0; i < files.length; i++) {
         var file = files[i];
-        var fullPath = path.resolve(dir, file);
-        var stat = fs.lstatSync(fullPath);
+        var fullname = path.resolve(dir, file);
+        var stat = fs.lstatSync(fullname);
         if (stat.isDirectory()) {
-            parseDir(fullPath,fileFullPaths);
+            parseDir(fullname);
         } else {
             if (file.indexOf(".") == 0) {
                 console.info("[info]", "过滤掉以 . 开头的File");
             // } else if (path.parse(fullPath).ext == ".ts") {
                 // console.log("[info]", file, "is ts file");
             } else {
-                fileFullPaths.push({fullname:fullPath});
+                filesData.files.push(
+                    {
+                        idIndex:filesData.files.length,
+                        fullname:fullname,
+                        curr_name:path.parse(fullname).name
+                    }
+                );
             }
         }
     }
 }
 
 app.get('/', function(req, res) {
-	// res.render('home');
-	res.render('change_file_name',getFilesData());
+    res.redirect(303, '/ChangeFileName');
 });
+app.get('/ChangeFileName',function(req,res){
+    res.render('change_file_name',getFilesData());
+});
+app.post('/ChangeFileName/change',function(req,res){
+    console.log("/ChangeFileName/change post:",req.body);
+    var fileVo = filesData.files[req.body.idIndex];
+    var parsedPath = path.parse(fileVo.fullname);
+    var newFullname = path.resolve(parsedPath.dir, req.body.newName)+parsedPath.ext;
+    console.log("newFullname:",newFullname);
+    fs.rename(fileVo.fullname,newFullname);
+    fileVo.fullname = newFullname;
+    filevo.curr_name = newFullname;
+    res.send({ success: true });
+});
+
 app.get('/about', function(req,res){
 	res.render('about', { 
 		fortune: fortune.getFortune(),
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,97 @@
+var filesLen;
+function init(_filesLen) {
+    console.log("{filesLen}", _filesLen);
+    filesLen = _filesLen;
+}
+function itemMouseOver(idIndex) {
+    // console.log("btn_over "+id);
+    // $("#div_item_"+id).find(".new_name").append("I would like to say:");
+    // $("#div_item_"+id).find(".new_name").addClass("bg_yellow");
+    $("#div_item_" + idIndex).addClass("bg_yellow");
+}
+function itemMouseOut(idIndex) {
+    $("#div_item_" + idIndex).removeClass("bg_yellow");
+}
 
+function onSelectAll() {
+    var isSelectedAll = true;
+    for (var i = 0; i < filesLen; i++) {
+        var currSelected = $("#div_item_" + i).find(".checkbox").prop("checked");
+        console.log(i, currSelected);
+        if (currSelected == false) {
+            $("#div_item_" + i).find(".checkbox").prop("checked", true);
+            isSelectedAll = false;
+        }
+    }
+    if (isSelectedAll) {
+        for (i = 0; i < filesLen; i++) {
+            $("#div_item_" + i).find(".checkbox").prop("checked", false);
+        }
+    }
+}
+function onResetName(idIndex) {
+    var div = $("#div_item_" + idIndex);
+    div.find(".preview_name_input").prop("value", div.find(".curr_name").text());
+}
+function onBtnClick(kind, idIndex) {
+    var div = $("#div_item_" + idIndex);
+    if (kind == 0) {
+        if(div.find(".curr_name").text() == div.find(".preview_name_input").prop("value")){
+            alert("Name have not changed");
+        }else{
+            $.ajax({
+                url: "/ChangeFileName/change",
+                type: 'POST',
+                data: "idIndex="+idIndex+"&newName="+div.find(".preview_name_input").prop("value"),
+                // data: "{idIndex:"+idIndex+"}",
+                success: function(data){
+                    div.find(".curr_name").text(div.find(".preview_name_input").prop("value"));
+                },
+                error: function(){
+                    console.log("ajax error");
+                }
+            });
+        }
+    } else {
+        var currName = div.find(".preview_name_input").prop("value");
+        div.find(".preview_name_input").prop("value", changeName(kind, currName));
+    }
+}
+
+function changeName(kind, currName) {
+    switch (kind) {
+        case 1:
+            if (currName.indexOf("-") == -1) {
+                var reg = /[^0-9]+/g;
+                var rs_unNum = reg.exec(currName);
+                reg = /[0-9]+/g;
+                var rs_num = reg.exec(currName);
+                return rs_unNum + "-" + rs_num;
+            }
+            break;
+        case 2:
+            return currName.toLowerCase();
+            break;
+        case 3:
+            var currNameSp = currName.split(" ");
+            if (currNameSp.length == 1) {
+                return;
+            } else {
+                var temp = currNameSp[1];
+                currNameSp[1] = currNameSp[0];
+                currNameSp[0] = temp;
+                return currNameSp.join(" ");
+            }
+            break;
+        case 4:
+            var currNameSp = currName.split(" ");
+            if (currNameSp.length == 1) {
+                return;
+            } else {
+                currNameSp.reverse();
+                return currNameSp.join(" ");
+            }
+            break;
+    }
+    return currName;
+}
