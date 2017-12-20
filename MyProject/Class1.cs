2e3fd6138d2c57e49b937f771d418578abdb97d9
EnderDFox <232623741@qq.com>
2017-12-20 14:55:44 +0800

    Razor

--- MyProject/Class1.cs
+++ MyProject/Class1.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Web.Razor;
+using RazorEngine;
+using RazorEngine.Templating;
 
 namespace MyProject
 {
+    // public class Model1
+    // {
+        // string Name = "ww";
+    // }
     public class Class1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! MyProject");
+            var template = "Hello @Model.Name, welcome to use RazorEngine!";
+            var result = Engine.Razor.RunCompile(template, "templateKey1", null, new { Name = "World" });
+            Console.WriteLine(result);
+            Console.Read();
         }
-        public static int check(int val){
+        public static int check(int val)
+        {
             return val;
         }
     }
