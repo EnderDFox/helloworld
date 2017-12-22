19d0917953dc9b773612fd5876d2def8b9cdd421
EnderDFox <232623741@qq.com>
2017-12-22 16:32:53 +0800

    init

---
+++ Assets/scripts/Script1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour {
+
+    float _OffsetUV = 0f;
+    SkinnedMeshRenderer mr;
+    // Use this for initialization
+    void Start () {
+        this.mr = this.GetComponentInChildren<SkinnedMeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _OffsetUV = Mathf.Repeat(_OffsetUV - 0.005f, 1f) ;
+        this.mr.material.SetFloat("_OffsetUV", _OffsetUV);
+    }
+}
