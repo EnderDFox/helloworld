b2d9ea03b779eb0a6848c9f1e3138b9646ebc2f1
EnderDFox <232623741@qq.com>
2017-12-24 14:55:19 +0800

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var child_process = require("child_process");
+gulp.task('tsc StaticServer', function (cb) {
+    var tsProject = ts.createProject('tsconfig.server.json');
+    var tsResult = gulp.src("src/StaticServer.ts").pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest('bin/js'));
+});
+// gulp.task('launch StaticServer', ['tsc StaticServer'],function (cb) {
+gulp.task('launch StaticServer', function (cb) {
+    // console.log('launch StaticServer');//
+    var free = child_process.spawn('node', ['bin/js/StaticServer.js']);
+    // 捕获标准输出并将其打印到控制台 
+    free.stdout.on('data', function (data) {
+        console.log('' + data); //data直接输出是奇怪的字符,所以这里先用''+转一下
+        // console.log(data);
+    });
+    // 捕获标准错误输出并将其打印到控制台 
+    free.stderr.on('data', function (data) {
+        // console.log('standard error output:\n' + data);
+        console.log(data);
+    });
+    // 注册子进程关闭事件 
+    free.on('exit', function (code, signal) {
+        console.log('child process eixt ,exit:' + code);
+        cb();
+    });
+});
+gulp.task('default', ['launch StaticServer'], function () {
+});
+//# sourceMappingURL=gulpfile.js.map
---
+++ src/TestA.ts
@@ -0,0 +1,3 @@
+var a:number = 1;
+var b:number = 2;
+console.log(a+b);
