c12611cb0615521cbd83f33aa0afa5b19c10cbec
EnderDFox <232623741@qq.com>
2018-01-25 11:05:33 +0800

    试delete dictionary key in forin

---
+++ src/Test1.ts
@@ -0,0 +1,27 @@
+var a:{[key:number]:boolean} = {};
+a["e"] = true;
+a["f"] = true;
+a["g"] = true;
+a["h"] = true;
+var b:{u:string,v:number,k:boolean} = {u:"this is u",v:94011,k:true};
+
+for (const key in a) {
+    const element = a[key];
+    console.log(key,element);
+    delete a["e"];
+    delete a["f"];
+    delete a["g"];
+}
+console.log("[debug]","----------");
+for (const key in b) {
+    const element = b[key];
+    console.log(key,element);
+}
+console.log("[debug]","----------");
+delete b.u;
+delete b["v"];
+
+for (const key in b) {
+    const element = b[key];
+    console.log(key,element);
+}
